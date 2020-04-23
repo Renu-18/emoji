64ac63b7890d982aac8570ae19d6f8b4a5b87717
Renu-18 <63661956+Renu-18@users.noreply.github.com>
2020-04-23 20:03:08 +0530

    Add files via upload

---
+++ smileyy.js
@@ -0,0 +1,414 @@
+var obj=document.querySelector("canvas");
+obj.width=window.innerWidth;
+obj.height=window.innerHeight;
+var txt=obj.getContext('2d');
+
+//circle
+txt.beginPath()
+txt.arc(850,250,50,0,Math.PI*2,false);
+/*
+1.leftorright
+2.up or down
+3.width
+4.circle size
+*/
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="yellow";//gap color
+txt.fill();
+//inside simley
+//1 smiley
+//eye
+txt.beginPath()
+txt.arc(830,240,8,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+txt.beginPath()
+txt.arc(870,240,8,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+
+//mouth
+txt.beginPath()
+txt.arc(850,280,10,3,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+
+//circle-2
+txt.beginPath()
+txt.arc(750,350,50,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="yellow";//gap color
+txt.fill();
+
+//2 smiley
+txt.beginPath()
+txt.arc(770,340,12,0,Math.PI*2,false);
+txt.strokeStyle="white";
+txt.stroke();
+txt.fillStyle="white";//gap color
+txt.fill();
+
+txt.beginPath()
+txt.arc(730,340,12,0,Math.PI*2,false);
+txt.strokeStyle="white";
+txt.stroke();
+txt.fillStyle="white";//gap color
+txt.fill();
+
+
+txt.beginPath()
+txt.arc(730,340,5,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+txt.beginPath()
+txt.arc(770,340,5,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+
+
+//mouth
+txt.beginPath()
+txt.arc(750,380,10,3,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+
+
+//circle-3
+txt.beginPath()
+txt.arc(670,250,50,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="yellow";//gap color
+txt.fill();
+
+//3 smiley
+txt.beginPath()
+txt.arc(690,235,8,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+txt.beginPath()
+txt.arc(650,235,8,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+
+
+
+
+
+
+//circle-4
+txt.beginPath()
+txt.arc(670,450,50,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="yellow";//gap color
+txt.fill();
+
+//4 smiley
+txt.beginPath()
+txt.arc(690,435,12,0,Math.PI*2,false);
+txt.strokeStyle="white";
+txt.stroke();
+txt.fillStyle="white";//gap color
+txt.fill();
+
+txt.beginPath()
+txt.arc(685,438,5,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+
+
+txt.beginPath()
+txt.arc(650,435,12,0,Math.PI*2,false);
+txt.strokeStyle="white";
+txt.stroke();
+txt.fillStyle="white";//gap color
+txt.fill();
+
+
+txt.beginPath()
+txt.arc(655,440,5,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+
+
+
+
+//mouth
+txt.beginPath()
+txt.arc(670,480,10,3,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+//circle-5
+txt.beginPath()
+txt.arc(850,450,50,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="yellow";//gap color
+txt.fill();
+
+//5 smiley
+txt.beginPath()
+txt.arc(870,440,12,0,Math.PI*2,false);
+txt.strokeStyle="white";
+txt.stroke();
+txt.fillStyle="white";//gap color
+txt.fill();
+
+txt.beginPath()
+txt.arc(830,440,12,0,Math.PI*2,false);
+txt.strokeStyle="white";
+txt.stroke();
+txt.fillStyle="white";//gap color
+txt.fill();
+
+
+txt.beginPath()
+txt.arc(830,433,5,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+txt.beginPath()
+txt.arc(870,433,5,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+
+
+//mouth
+txt.beginPath()
+txt.arc(850,480,10,3,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+//circle
+txt.beginPath()
+txt.arc(350,250,50,0,Math.PI*2,false);
+/*
+1.leftorright
+2.up or down
+3.width
+4.circle size
+*/
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="yellow";//gap color
+txt.fill();
+//inside simley
+//1 smiley
+//eye
+txt.beginPath()
+txt.arc(330,240,10,3,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+txt.beginPath()
+txt.arc(370,240,10,3,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+
+//mouth
+txt.beginPath()
+txt.arc(350,270,7,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+
+//circle-2
+txt.beginPath()
+txt.arc(450,350,50,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="yellow";//gap color
+txt.fill();
+
+//2 smiley
+
+
+
+
+txt.beginPath()
+txt.arc(430,340,5,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+txt.beginPath()
+txt.arc(470,335,12,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+txt.beginPath()
+txt.arc(470,335,4,0,Math.PI*2,false);
+txt.strokeStyle="white";
+txt.stroke();
+txt.fillStyle="white";//gap color
+txt.fill();
+
+
+
+
+
+//mouth
+txt.beginPath()
+txt.arc(450,380,10,3,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+
+
+//circle-4
+txt.beginPath()
+txt.arc(550,250,50,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="yellow";//gap color
+txt.fill();
+
+//4 smiley
+//line
+txt.beginPath()
+txt.moveTo(560,235);//starting point 700 to 900&200 to300
+txt.lineTo(580,235);
+txt.strokeStyle="black";
+txt.stroke();
+
+txt.beginPath()
+txt.moveTo(520,235);//starting point 700 to 900&200 to300
+txt.lineTo(540,235);
+txt.strokeStyle="black";
+txt.stroke();
+
+txt.beginPath()
+txt.moveTo(539,275);//starting point 700 to 900&200 to300
+txt.lineTo(560,275);
+txt.strokeStyle="black";
+txt.stroke();
+
+
+
+
+
+
+//circle-5
+txt.beginPath()
+txt.arc(550,450,50,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="yellow";//gap color
+txt.fill();
+
+//5 smiley
+txt.beginPath()
+txt.arc(530,440,8,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+txt.beginPath()
+txt.arc(570,440,8,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+
+
+
+//mouth
+//line
+txt.beginPath()
+txt.moveTo(540,475);//starting point 700 to 900&200 to300
+txt.lineTo(560,475);
+txt.strokeStyle="black";
+txt.stroke();
+
+
+
+//circle-2
+txt.beginPath()
+txt.arc(350,450,50,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="yellow";//gap color
+txt.fill();
+
+//2 smiley
+
+txt.beginPath()
+txt.arc(330,440,5,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+txt.beginPath()
+txt.arc(370,440,5,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
+
+
+
+//mouth
+txt.beginPath()
+txt.arc(350,480,7,0,Math.PI*2,false);
+txt.strokeStyle="black";
+txt.stroke();
+txt.fillStyle="black";//gap color
+txt.fill();
